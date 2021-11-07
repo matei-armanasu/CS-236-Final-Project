b648c613060ea9a2d7cb15ea9092cceee3a267e8
matei-armanasu <71412498+matei-armanasu@users.noreply.github.com>
2021-11-07 01:01:25 -0800

    Created generator loss function

---
+++ utils/loss.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+# TODO: test that this is accurate and gives us the loss that we expect. Want cross-entropy between the
+#       target adversarial distribution and the discriminator output, as well as a MSE between the true 
+#       image and the generated image.
+def generator_loss(images, generated_images, discr_output, y, beta):
+    sm_output = nn.Softmax(-1)(discr_output)
+
+    cce_loss = torch.mean(torch.sum(-torch.mul(torch.log(sm_output),y), dim=1))
+
+    mse_loss = torch.mean(torch.sum(torch.nn.MSELoss(reduction='none')(images, generated_images), dim=(1, 2, 3)))
+    return cce_loss + beta * mse_loss
