199db66919d06b9a72a595cfda96354415507ce9
Matei Armanasu <armanasu@stanford.edu>
2021-11-08 23:17:52 -0800

    created classifier/dataloader files

---
+++ models/classifier.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+def pretrained_resnet_18():
+    model_ft = models.resnet18()
+    #Finetune Final few layers to adjust for tiny imagenet input
+    model_ft.conv1 = nn.Conv2d(3,64, kernel_size=(3,3), stride=(1,1), padding=(1,1), bias=False)
+    model_ft.maxpool = nn.Sequential()
+    model_ft.avgpool = nn.AdaptiveAvgPool2d(1)
+    model_ft.fc.out_features = 200
+
+    # Loading weights from pretrained model
+    pretrained_dict = torch.load('checkpoints/resnet18-f37072fd.pth')
+    model_ft_dict = model_ft.state_dict()
+    first_layer_weight = model_ft_dict['conv1.weight']
+    pretrained_dict = {b[0]:b[1] for a,b in zip(model_ft_dict.items(), pretrained_dict.items()) if a[1].size() == b[1].size()}
+    model_ft_dict.update(pretrained_dict) 
+    model_ft.load_state_dict(model_ft_dict)
+    
+    return model_ft
---
+++ utils/dataloader.py
@@ -0,0 +1,35 @@
+import os
+
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+def get_loader(which_loader, batch = 100, data_dir = "Imagenet"):
+    
+    data_transforms = {
+    'train': transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]),
+    ]),
+    'val': transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]),
+    ]),
+    'test': transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]),
+    ])
+    }   
+    
+    # num_workers = {
+    #     'train' : 100,
+    #     'val'   : 0,
+    #     'test'  : 0
+    # }
+    image_datasets = {x: datasets.ImageFolder(os.path.join("..",data_dir, x),
+                                          data_transforms[x])
+                        for x in ['train', 'val','test']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch,
+                                             shuffle=True, num_workers=1)
+              for x in ['train', 'val', 'test']}
+    return dataloaders[which_loader]
